be39e814a2dcdfa64d64685d0375849cb41685b7
Zeynep Yaşar <zynpsema@gmail.com>
2023-08-05 23:10:42 +0300

    Fixed bugs
    OrderHub file CurrentUserManager added

---
+++ CapstoneProject/CapstoneProject-Backend/src/WebApi/Services/BlazoredToastService.cs
@@ -0,0 +1,25 @@
+﻿
+using Application.Common.Interfaces;
+
+namespace Wasm.Services
+{
+    public class BlazoredToastService:IToasterService
+    {
+        private readonly IToasterService _toastService; //IToastService
+
+        public BlazoredToastService(IToasterService toastService) //IToastService
+        {
+            _toastService = toastService;
+        }
+
+        public void ShowSuccess(string message)
+        {
+            _toastService.ShowSuccess(message);
+        }
+
+        public void ShowError(string message)
+        {
+            _toastService.ShowError(message);
+        }
+    }
+}
